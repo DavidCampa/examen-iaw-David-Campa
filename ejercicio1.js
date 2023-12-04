6c6276381985b8b3af548c0bec95c662ce070ba4
david <davidcampa7@gmail.com>
2023-12-04 14:20:11 +0100

    js add

---
+++ ejercicio1.js
@@ -0,0 +1,13 @@
+const pt =require("prompt-sync")({signit: true})
+
+let numInput
+let num
+
+
+const aleatorio = Math.round(Math.random()*10)
+do { let numero = pt ("Cuantos dados quieres tirar: ")
+console.log (numero)
+if (Number(numInput)) {
+    num = Number(numInput)
+}
+}
---
+++ ejercicio2.js
@@ -0,0 +1,52 @@
+const pt =require("prompt-sync")({signit: true})
+
+let cadenas = pt("Dime una cadena de texto")
+let cadena = ""
+let consonantes
+let vocales = 0 
+
+while (true){
+    
+    for (let x = 0; x < vocales.length; x++){
+        if (String(cadenas))
+        consonantes = String(cadenas)
+        cadena = cadena + consonante
+        vocales.charAt(x) === "b" || 
+        vocales.charAt(x) === "c" ||
+        vocales.charAt(x) === "d" ||
+        vocales.charAt(x) === "f" ||
+        vocales.charAt(x) === "g" ||
+        vocales.charAt(x) === "h" ||
+        vocales.charAt(x) === "j" ||
+        vocales.charAt(x) === "k" ||
+        vocales.charAt(x) === "l" ||
+        vocales.charAt(x) === "m" ||
+        vocales.charAt(x) === "n" ||
+        vocales.charAt(x) === "ñ" ||
+        vocales.charAt(x) === "p" ||
+        vocales.charAt(x) === "q" ||
+        vocales.charAt(x) === "r" ||
+        vocales.charAt(x) === "s" ||
+        vocales.charAt(x) === "t" ||
+        vocales.charAt(x) === "v" ||
+        vocales.charAt(x) === "w" ||
+        vocales.charAt(x) === "x" ||
+        vocales.charAt(x) === "y" ||
+        vocales.charAt(x) === "z" ||
+        vocales.charAt(x) === "0" ||
+        vocales.charAt(x) === "1" ||
+        vocales.charAt(x) === "2" ||
+        vocales.charAt(x) === "3" ||
+        vocales.charAt(x) === "4" ||
+        vocales.charAt(x) === "5" ||
+        vocales.charAt(x) === "6" ||
+        vocales.charAt(x) === "7" ||
+        vocales.charAt(x) === "8" ||
+        vocales.charAt(x) === "9" 
+
+        {
+            vocales = vocales +1
+        }
+    }
+}
+console.log("las consonantes son --> " + cadenas)
